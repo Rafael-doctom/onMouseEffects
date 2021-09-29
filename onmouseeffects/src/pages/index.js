b1cda3524582a6f52a7ca56928f19b62d86e259c
Rafael Pinto da Silva <dasilva.rafaelpt@gmail.com>
2021-09-29 15:54:21 -0300

    update index

--- onmouseeffects/src/pages/index.js
+++ onmouseeffects/src/pages/index.js
@@ -1,7 +1,11 @@
 import Head from 'next/head'
 import OnClickMouse from '../../src/pages/components/OnClickMouse/OnClickMouse'
+import OnMouseLeave from './components/OnMouseLeave/OnMouseLeave'
 export default function Home() {
   return (
-    <OnClickMouse />
+    <>
+      <OnClickMouse />
+      <OnMouseLeave />
+    </>
   )
 }
