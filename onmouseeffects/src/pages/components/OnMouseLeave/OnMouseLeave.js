da15107309996b2017d35da924bfcab08577f2a4
Rafael Pinto da Silva <dasilva.rafaelpt@gmail.com>
2021-09-30 09:41:31 -0300

    click effect update

--- onmouseeffects/src/pages/components/OnMouseLeave/OnMouseLeave.js
+++ onmouseeffects/src/pages/components/OnMouseLeave/OnMouseLeave.js
@@ -1,53 +1,90 @@
 import React, { useState } from 'react'
 
 
+import styled from 'styled-components'
+const Items = styled.div`
+    text-align:center;
+`
+
+const TextCenter = styled.div`
+    text-align:center;
+    font-size:2rem;
+    padding:1rem;
+    `
+const ItemsList = styled.div`
+    text-align:center;
+    font-size:2rem;
+
+    .lista{
+        color:gray;
+        display:flex;
+        justify-content:center;
+    }
+    .lista li{
+        font-size:15px;
+        list-style:none; 
+        margin-left:1rem;
+        list-style:none;
+    }
+   
+`
 export default function OnMouseLeave() {
     const [sobreModal, setSobreModal] = useState(false)
+    const [habilidadesModal, setHabilidadesModal] = useState(false)
+    const [contatosModal, setContatosModal] = useState(false)
+
+
     return (
         <>
-            <div>
+            <Items>
                 <h4>OnMouseLeaveuseLeave</h4>
 
-                <div>
-                    <ul>
+                <ItemsList>
+                    <ul class="lista">
                         <div>
                             <li onMouseEnter={() => setSobreModal(true)}>
                                 Sobre
                             </li>
-                            {
-                                sobreModal ?
-                                    <div>
-                                        teste
-                                    </div>
-                                    : null
-                            }
+
+                            <p onClick={() => setSobreModal(false)}>fechar</p>
                         </div>
+                        {
+                            sobreModal ?
+                                <h1>sobre aqui</h1>
+                                : null
+                        }
                         <div>
-                            <li onMouseEnter={() => setSobreModal(true)}>
+
+                            <li onMouseEnter={() => setHabilidadesModal(true)}>
                                 Habilidades
                             </li>
-                            {
-                                sobreModal ?
-                                    <div>
-                                        teste
-                                    </div>
-                                    : null
-                            }
+
+                            <p onMouseLeave={() => setHabilidadesModal(false)}>fechar</p>
+
+
                         </div>
+
+                        {
+                            habilidadesModal ?
+                                <h1>habilidades aqui</h1>
+                                : null
+                        }
                         <div>
-                            <li onMouseEnter={() => setSobreModal(true)}>
+                            <li onMouseEnter={() => setContatosModal(true)}>
                                 Contato
                             </li>
-                            {
-                                sobreModal ?
-                                    <div>
-                                        teste
-                                    </div>
-                                    : null
-                            }
+
+                            <p onClick={() => setContatosModal(false)}>fechar</p>
                         </div>
+                        {
+                            contatosModal ?
+                                <h1>Contato aqui</h1>
+                                : null
+                        }
+
                     </ul>
-                </div>
-            </div>        </>
+                </ItemsList>
+            </Items>
+        </>
     )
 }
